17a70edad45b152f3ed0d9f8bed994e39390eee6
Charlotte Amy Cuffe <109796603+charlocu@users.noreply.github.com>
2023-06-13 18:34:52 +0100

    updated nextera adapter trimming 23_06_13

--- 02_Filtering.sh
+++ 02_Filtering.sh
@@ -11,13 +11,14 @@
 module load apps/anaconda-4.7.12.tcl
 eval "$(conda shell.bash hook)"
 conda activate preproc
+
 #removing the nextera adaptor sequence from the reads
 #next-seq trim specifically cause of two colour chemistry used in next-seq and nova-seq, and is a quality filtering step based on phred-33
 # -m is stating minimum read length, --max-n is the maximum amount of allowable N's called in the sequences
 # apparently -max-n and --nextseq-trim are only available in more recent versions
 #SCDM read1 & 2
 
-cutadapt -a CTGTCTCTTATACACATCT --nextseq-trim=30 --minimum-length=25 \
+cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT --nextseq-trim=30 --minimum-length=25 \
 --max-n 0 \
 -o /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SCDM_1.fq.gz \
 -p /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SCDM_2.fq.gz \
@@ -25,7 +26,7 @@ cutadapt -a CTGTCTCTTATACACATCT --nextseq-trim=30 --minimum-length=25 \
 /storage/users/ccuffe22/atac/raw_data/X204SC23050247-Z01-F001/01.RawData/WB135313_SCDM/WB135313_SCDM_EKDL230007343-1A_HF35WDSX7_L3_2.fq.gz
 
 #SM read1 & 2
-cutadapt -a CTGTCTCTTATACACATCT \
+cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT\
 --nextseq-trim=30 \
 --minimum-length=25 \
 --max-n 0 \
@@ -34,7 +35,7 @@ cutadapt -a CTGTCTCTTATACACATCT \
 /storage/users/ccuffe22/atac/raw_data/X204SC23050247-Z01-F001/01.RawData/WB135313_SM/WB135313_SM_EKDL230007342-1A_HF35WDSX7_L3_1.fq.gz \
 /storage/users/ccuffe22/atac/raw_data/X204SC23050247-Z01-F001/01.RawData/WB135313_SM/WB135313_SM_EKDL230007342-1A_HF35WDSX7_L3_2.fq.gz
 
-conda deactivate preproc
+conda deactivate 
 conda activate multiqc
 
 #fastqc of the above
@@ -46,4 +47,4 @@ fastqc /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SM_1
 fastqc /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SM_2.fq.gz
 multiqc -n -f adaptor_removal_multiqc /storage/users/ccuffe22/atac/data/03.pre-processing/
 
-conda deactivate multiqc
+conda deactivate 
