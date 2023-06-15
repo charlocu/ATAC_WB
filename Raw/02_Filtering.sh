4b53b284b982df1086704b8ec907819480408600
Charlotte Amy Cuffe <109796603+charlocu@users.noreply.github.com>
2023-06-15 15:54:12 +0100

    formattting 23_06-15

--- Raw/02_Filtering.sh
+++ Raw/02_Filtering.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --chdir=/storage/users/ccuffe22/atac/raw_data/
 #SBATCH --job-name=adapter_removal
-#SBATCH --output=/storage/users/ccuffe22/atac/raw_data/filtering_%j.log
+#SBATCH --output=/storage/users/ccuffe22/atac/logfiles/filtering_%j.log
 #SBATCH --mail-user=ccuffe22@rvc.ac.uk
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mem=1gb #Change this depending on memory requirements
@@ -18,7 +18,10 @@ conda activate preproc
 # apparently -max-n and --nextseq-trim are only available in more recent versions
 #SCDM read1 & 2
 
-cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT --nextseq-trim=30 --minimum-length=25 \
+cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT \
+-q 30 \
+#--nextseq-trim=30 \
+--minimum-length=25 \
 --max-n 0 \
 -o /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SCDM_1.fq.gz \
 -p /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SCDM_2.fq.gz \
@@ -27,7 +30,8 @@ cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT --nextseq-trim=30 --minim
 
 #SM read1 & 2
 cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT \
---nextseq-trim=30 \
+#--nextseq-trim=30 \
+-q 30 \
 --minimum-length=25 \
 --max-n 0 \
 -o /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SM_1.fq.gz \
@@ -36,7 +40,7 @@ cutadapt -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT \
 /storage/users/ccuffe22/atac/raw_data/X204SC23050247-Z01-F001/01.RawData/WB135313_SM/WB135313_SM_EKDL230007342-1A_HF35WDSX7_L3_2.fq.gz
 
 conda deactivate 
-conda activate multiqc
+#conda activate multiqc
 
 #fastqc of the above
 module load apps/fastqc-0.11.9.tcl
@@ -45,6 +49,6 @@ fastqc /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SCDM
 fastqc /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SCDM_2.fq.gz 
 fastqc /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SM_1.fq.gz
 fastqc /storage/users/ccuffe22/atac/data/03.pre-processing/trimmed_WB135313_SM_2.fq.gz
-multiqc -n -f adaptor_removal_multiqc /storage/users/ccuffe22/atac/data/03.pre-processing/
+#multiqc -n -f adaptor_removal_multiqc /storage/users/ccuffe22/atac/data/03.pre-processing/
 
-conda deactivate
+#conda deactivate
